316ce00aef31c59bdbc3d4df52de49443fc5f42d
DELL <karthikrmallaya@gmail.com>
2020-05-26 15:53:39 +0530

    seeding part for Producta and review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
@@ -7,6 +8,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+
+
+        'name'=> $faker->word,
+        'detail'=> $faker->paragraph,
+        'price'=> $faker->numberBetween(100,1000),
+        'stock'=> $faker->randomDigit,
+        'discount'=>$faker->numberBetween(2,30)
     ];
 });
